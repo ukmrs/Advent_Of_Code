0b37878631a2cc0404820b70adbfa0400cdfc153
ukmrs <ukmrs@protonmail.com>
2020-12-05 20:19:43 +0100

    day5 realization :flight_departure:

--- aoc2020/day5/solution.py
+++ aoc2020/day5/solution.py
@@ -1,42 +1,21 @@
 #!/usr/bin/env python3
-LOWER = {"F": True, "L": True, "B": False, "R": False}
-
-
-def get_rc(partitioning: str) -> int:
-    lb, hb = 0, 2**(len(partitioning)) - 1
-    for char in partitioning:
-        if LOWER[char]:
-            hb -= -((hb - lb) // -2)
-        else:
-            lb += -((hb - lb) // -2)
-    return lb
-
 
 with open("input") as f:
-    case = []
+    case = set()
+    to_binary = {ord(char): bl for char, bl in zip("BRFL", "1100")}
     for line in f.readlines():
-        line = line.strip()
-        row, col = get_rc(line[:7]), get_rc(line[7:])
-        case.append((row, col))
+        case.add(int(line.strip().translate(to_binary), 2))
 
 
 def part1(bpasses):
-    mx = float("-INF")
-    for bpass in bpasses:
-        row, col = bpass
-        mx = max(row * 8 + col, mx)
-    return mx
+    return max(bpasses)
 
 
 def part2(bpasses):
-    out_of_bounds = part1(bpasses)
-    max_row = out_of_bounds // 8
-    bset = {i[0] * 8 + i[1] for i in bpasses}
-    for row in range(max_row, -1, -1):
-        for col in range(8):
-            if (sid := row * 8 + col) not in bset:
-                if sid < out_of_bounds:  # max_row + higher col than possible
-                    return sid
+    max_sid = part1(bpasses)
+    for sid in range(max_sid, -1, -1):
+        if sid not in case:
+            return sid
 
 
 print(f"part1: {part1(case)}")  # 996
